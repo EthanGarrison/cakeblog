7fe845347d99de985c4d446f418ff9525febc26b
Ethan <emrys94@gmail.com>
2013-09-18 01:15:23 -0400

    Added 'Add Post' button, dont think it works

--- cake/app/Controller/PostsController.php
+++ cake/app/Controller/PostsController.php
@@ -18,6 +18,14 @@
 			}
 			$this->set('post', $post);
 		}
+		
+		public function add() {
+			if ($this->request->is('post')) {
+				$this->Session->setFlash(__('Your post has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Session->setFlash(__('Unable to add your post.'));
+		}
 	}
 	
 ?>
