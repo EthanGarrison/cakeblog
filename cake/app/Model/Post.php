c84d09f6c52a81a0199fd73905c07dfddbf9fa0c
EthanGarrison <emrys94@gmail.com>
2013-09-19 18:08:43 -0400

    Fixed Add Post stuff, starting on Comments

--- cake/app/Model/Post.php
+++ cake/app/Model/Post.php
@@ -1,14 +1,58 @@
 <?php
+App::uses('AppModel', 'Model');
+/**
+ * Post Model
+ *
+ * @property Comment $Comment
+ */
+class Post extends AppModel {
 
-	class Post extends AppModel {
-		public $validate = array(
-			'title' => array(
-				'rule' => 'notEmpty'
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'title';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'title' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
-			'body' => array(
-				'rule' => 'notEmpty'
-			)
-		);
-	}
-	
-?>
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Comment' => array(
+			'className' => 'Comment',
+			'foreignKey' => 'post_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
